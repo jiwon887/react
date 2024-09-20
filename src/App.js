56775bc939aa3eeebd61526045d09469f62319a2
jiwon <wldnjs4685@gmail.com>
2024-09-20 18:59:51 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  // useState 사용법 -> 변수명[변수, 변수 수정 함수] = useState(['a','b','c'])
+
+  let [title, setTitle] = useState('title A', 'title B', 'title C');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "box"> 
+        <h4>blog</h4> 
+      </div>
+      <div className = "list"> 
+        <h4> { title } </h4> 
+      </div>
+      <div className = "list"> 
+        <h4> { title } </h4> 
+      </div>
+      <div className = "list"> 
+        <h4> { title }}</h4> 
+      </div>
+      
     </div>
   );
 }
