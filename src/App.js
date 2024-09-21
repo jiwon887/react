32f2aa6dc2fb138450f803cd4d8e9d1d8d4ed85b
jiwon <wldnjs4685@gmail.com>
2024-09-21 17:55:45 +0900

    add modal

--- src/App.js
+++ src/App.js
@@ -32,7 +32,11 @@ function App() {
       <div className = "list"> 
         <h4> { title[2] } </h4> 
       </div>
-      
+      <div className='modal'>
+        <h4> Title</h4>
+        <p> Date </p>
+        <p> summary </p>
+      </div>
     </div>
   );
 }
